a3ac1da06070b9f0c918fea999e8ccdb27e1c99d
David Březina <david@rosettatype.com>
2022-03-18 14:47:57 +0100

    More asset renaming

--- assets/2022-03-21-elements-of-multi-script-typography-chapter-2/convert-figures-pdf2svg.sh
+++ assets/2022-03-21-elements-of-multi-script-typography-chapter-2/convert-figures-pdf2svg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-pdf2svg figures_pt2.pdf keystroke-sequences.svg 1
-pdf2svg figures_pt2.pdf english-word.svg 2
-pdf2svg figures_pt2.pdf paragraph-directions.svg 3
-pdf2svg figures_pt2.pdf shaping-arabic.svg 4
-pdf2svg figures_pt2.pdf shaping-ligatures.svg 5
-pdf2svg figures_pt2.pdf shaping-reordering.svg 6
-pdf2svg figures_pt2.pdf glyph-boundaries.svg 7
-pdf2svg figures_pt2.pdf kerning.svg 8
-pdf2svg figures_pt2.pdf mark-positioning.svg 9
-pdf2svg figures_pt2.pdf cursive-attachment.svg 10
+pdf2svg figures_ch2.pdf keystroke-sequences.svg 1
+pdf2svg figures_ch2.pdf english-word.svg 2
+pdf2svg figures_ch2.pdf paragraph-directions.svg 3
+pdf2svg figures_ch2.pdf shaping-arabic.svg 4
+pdf2svg figures_ch2.pdf shaping-ligatures.svg 5
+pdf2svg figures_ch2.pdf shaping-reordering.svg 6
+pdf2svg figures_ch2.pdf glyph-boundaries.svg 7
+pdf2svg figures_ch2.pdf kerning.svg 8
+pdf2svg figures_ch2.pdf mark-positioning.svg 9
+pdf2svg figures_ch2.pdf cursive-attachment.svg 10
--- assets/2022-03-28-elements-of-multi-script-typography-chapter-3/convert-figures-pdf2svg.sh
+++ assets/2022-03-28-elements-of-multi-script-typography-chapter-3/convert-figures-pdf2svg.sh
@@ -2,8 +2,8 @@
 
 pdf2svg bidi-input.pdf bidi-input.svg 1
 
-pdf2svg figures_pt3.pdf paragraph-directions.svg 1
-pdf2svg figures_pt3.pdf paragraph-alignments.svg 2
-pdf2svg figures_pt3.pdf justification.svg 3
-pdf2svg figures_pt3.pdf different-line-heights.svg 4
-pdf2svg figures_pt3.pdf bidi-hyphenation.svg 5
+pdf2svg figures_ch3.pdf paragraph-directions.svg 1
+pdf2svg figures_ch3.pdf paragraph-alignments.svg 2
+pdf2svg figures_ch3.pdf justification.svg 3
+pdf2svg figures_ch3.pdf different-line-heights.svg 4
+pdf2svg figures_ch3.pdf bidi-hyphenation.svg 5
