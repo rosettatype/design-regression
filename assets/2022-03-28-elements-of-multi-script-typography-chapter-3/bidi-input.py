ca45411652853b397adc40e33305055cb7a659b2
David Březina <david@rosettatype.com>
2022-03-18 17:23:24 +0100

    EoMST 3: figure update

--- assets/2022-03-28-elements-of-multi-script-typography-chapter-3/bidi-input.py
+++ assets/2022-03-28-elements-of-multi-script-typography-chapter-3/bidi-input.py
@@ -1,28 +1,28 @@
-newPage(400, 625)
-font("AdapterArabicDisplay-Rg")
+import drawBot as db
+
+db.newPage(400, 625)
+db.font("AdapterArabicDisplay-Rg")
 fsize = 30
-fontSize(fsize)
-fill(0)
-strokeWidth(1)
+db.fontSize(fsize)
+db.fill(0)
+db.strokeWidth(1)
 word = "بريد الالكتروني:"
 additional = " john@doe.com"
 add = ""
-w = width()
-translate(w, height() - 50)
+w = db.width()
+db.translate(w, db.height() - 50)
 for i, a in enumerate(additional):
     add += a
-    tw, _ = textSize(word + add)
-    
-    fill(1)
-    stroke(0)
-    rect(-1, -10, -w+2, fsize + 10)
-    stroke(237/255, 28/255, 36/255)
+    tw, _ = db.textSize(word + add)
+    db.fill(1)
+    db.stroke(0)
+    db.rect(-1, -10, -w+2, fsize + 10)
+    db.stroke(237/255, 28/255, 36/255)
     x = -tw - 30
-    line((x, -5), (x, fsize - 5))
-    fill(0)
-    stroke(None)
-    text(word + add, (-25, 0), align="right")
-    
-    translate(0, -45)
+    db.line((x, -5), (x, fsize - 5))
+    db.fill(0)
+    db.stroke(None)
+    db.text(word + add, (-25, 0), align="right")
+    db.translate(0, -45)
 
-saveImage("bidi-input.pdf")
+db.saveImage("bidi-input.pdf")
